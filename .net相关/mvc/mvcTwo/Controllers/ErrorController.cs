9ff8b7b1f7c5f6c4b5ba5688a2c2eac8ac1af2ad
shuaiagain <764258872@qq.com>
2018-06-21 09:15:38 +0800

    1.新增内容

---
+++ .net相关/mvc/mvcTwo/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcTwo.Controllers
+{
+    [AllowAnonymous]
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            Exception e = new Exception("Invalid Controller or/and Action Name");
+            HandleErrorInfo eInfo = new HandleErrorInfo(e, "Unknown", "Unknown");
+            return View("Error", eInfo);
+        }
+    }
+}
---
+++ .net相关/mvc/mvcTwo/Filters/EmployeeExceptionFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using mvcTwo.Logger;
+
+namespace mvcTwo.Filters
+{
+    public class EmployeeExceptionFilter : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            FileLogger log = new FileLogger();
+
+            log.LogException(filterContext.Exception);
+            base.OnException(filterContext);
+        }
+    }
+}
